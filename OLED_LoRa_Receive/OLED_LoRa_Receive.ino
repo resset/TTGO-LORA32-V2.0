bcea642550926f500e8ac6366a947881fa9dacf5
Mateusz Tomaszkiewicz <ressetpublic@gmail.com>
2019-02-12 00:39:00 +0100

    Change default frequency to 433 MHz
    Also, fix some formatting issues.

--- OLED_LoRa_Receive/OLED_LoRa_Receive.ino
+++ OLED_LoRa_Receive/OLED_LoRa_Receive.ino
@@ -10,13 +10,12 @@
 #define SS      18   // GPIO18 -- SX1278's CS
 #define RST     14   // GPIO14 -- SX1278's RESET
 #define DI0     26   // GPIO26 -- SX1278's IRQ(Interrupt Request)
-#define BAND    868E6
+#define BAND    433E6
 
 SSD1306 display(0x3c, 21, 22);
 String rssi = "RSSI --";
 String packSize = "--";
 String packet ;
-
 
 void loraData(){
   display.clear();
@@ -41,15 +40,16 @@ void setup() {
   pinMode(16,OUTPUT);
   digitalWrite(16, LOW);    // set GPIO16 low to reset OLED
   delay(50); 
-  digitalWrite(16, HIGH); // while OLED is running, must set GPIO16 in high、
+  digitalWrite(16, HIGH); // while OLED is running, must set GPIO16 in high
   
   Serial.begin(115200);
   while (!Serial);
   Serial.println();
   Serial.println("LoRa Receiver Callback");
+
   SPI.begin(SCK,MISO,MOSI,SS);
-  LoRa.setPins(SS,RST,DI0);  
-  if (!LoRa.begin(868E6)) {
+  LoRa.setPins(SS,RST,DI0);
+  if (!LoRa.begin(433E6)) {
     Serial.println("Starting LoRa failed!");
     while (1);
   }
--- OLED_LoRa_Sender/OLED_LoRa_Sender.ino
+++ OLED_LoRa_Sender/OLED_LoRa_Sender.ino
@@ -5,12 +5,12 @@
 #include "images.h"
 
 #define SCK     5    // GPIO5  -- SX1278's SCK
-#define MISO    19   // GPIO19 -- SX1278's MISnO
+#define MISO    19   // GPIO19 -- SX1278's MISO
 #define MOSI    27   // GPIO27 -- SX1278's MOSI
 #define SS      18   // GPIO18 -- SX1278's CS
 #define RST     14   // GPIO14 -- SX1278's RESET
 #define DI0     26   // GPIO26 -- SX1278's IRQ(Interrupt Request)
-#define BAND  868E6
+#define BAND    433E6
 
 unsigned int counter = 0;
 
@@ -19,12 +19,9 @@ String rssi = "RSSI --";
 String packSize = "--";
 String packet ;
 
- 
-
 void setup() {
   pinMode(16,OUTPUT);
   pinMode(2,OUTPUT);
-  
   digitalWrite(16, LOW);    // set GPIO16 low to reset OLED
   delay(50); 
   digitalWrite(16, HIGH); // while OLED is running, must set GPIO16 in high
@@ -33,10 +30,10 @@ void setup() {
   while (!Serial);
   Serial.println();
   Serial.println("LoRa Sender Test");
-  
+
   SPI.begin(SCK,MISO,MOSI,SS);
   LoRa.setPins(SS,RST,DI0);
-  if (!LoRa.begin(868E6)) {
+  if (!LoRa.begin(433E6)) {
     Serial.println("Starting LoRa failed!");
     while (1);
   }
